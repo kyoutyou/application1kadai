00c41133d8117f20f44a1e526c2eb0ec71d6bf1b
EC2 Default User <ec2-user@ip-172-31-1-137.ap-northeast-1.compute.internal>
2022-08-30 14:53:15 +0000

    [Add]viewsとcontrollers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,43 @@
 class BooksController < ApplicationController
-  
+
   def new
   end
 
   def index
     @books = Book.all
+    @book=Book.new
   end
 
   def show
-    @books = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), edit_book_path(@book.id)
+    else
+      @books=Book.all
+      render :index,:edit
+    end
+    
+    def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to books_path(book.id)  
+    end
   end
-
+ 
   private
   def book_params
     params.require(:book).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources:homes,:books
+  resources:books
   root to:'homes#top'
 end
